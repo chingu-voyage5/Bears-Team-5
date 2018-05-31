eeb6911710179b6a8b8ca1f8b12edd6ed909d8a9
Mariano Mariano <marianofheller@gmail.com>
2018-05-30 23:01:51 -0300

    Added client dependencies, config redux with sagas

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+
+const store = configureStore();
+
+
+render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
