030725501d10219d22da9433e3e9a15f5659a8c1
ziggysauce <dtn3423@gmail.com>
2018-06-02 17:33:16 -0400

    remove server folder

---
+++ middleware/session.js
@@ -0,0 +1,8 @@
+const session = require('express-session');
+
+module.exports = session({
+  resave: true,
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: true,
+  cookie: { maxAge: 24 * 60 * 60 * 1000 }, // cookie age of 1 day
+});
