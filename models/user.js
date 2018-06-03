3543b168ed37bcc30c9d660b3b94524c9f9dd7bc
Madison Crocker <m.voltaire.c@gmail.com>
2018-06-03 16:57:43 -0400

    chore: Set up base files for migrations

---
+++ models/user.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
