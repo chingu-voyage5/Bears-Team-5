8f7e39c6279ea03aaf7f98c3823e81e8796e0dab
Mariano Mariano <marianofheller@gmail.com>
2018-06-09 17:45:05 -0300

    Container + redux stuff

---
+++ client/src/containers/Goals/StyledComponents.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+
+export const Title = styled.div`
+  font-size: 1.5rem;
+  justify-content: center;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+export const GoalsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20vh;
+  margin-left: 40vw;
+  margin-right: 40vw;
+
+  @media only screen and (max-width : 1024px) {
+    margin-left: 35vw;
+    margin-right: 35vw;
+  }
+  @media only screen and (max-width : 768px) {
+    margin-left: 30vw;
+    margin-right: 30vw;
+  }
+  @media only screen and (max-width : 320px) {
+    margin-left: 10vw;
+    margin-right: 10vw;
+  }
+`;
+
+
+export const GoalsListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Error = styled.div`
+  font-size: 0.75rem;
+  color: ${({ theme }) => theme.error};
+  text-align: center;
+`;
+
+
+export const SpinnerContainer = styled.div`
+  color: ${({ theme }) => theme.error};
+  text-align: center;
+`;
---
+++ client/src/containers/Goals/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Spinner from '../../components/Spinner';
+import * as SC from './StyledComponents';
+import * as actions from '../../actions/goals';
+
+class Goals extends React.Component {
+  componentDidMount() {
+    const { getDailyGoals } = this.props;
+    getDailyGoals();
+  }
+
+  render() {
+    const { isFetching, errors, dailyGoals } = this.props;
+    return (
+      <SC.GoalsContainer>
+        <SC.Title>goals</SC.Title>
+        <SC.GoalsListContainer>
+          <p>TODO: INSERT LIST COMPONENT HERE</p>
+          {dailyGoals.map((goal, i) => <p key={i}>{goal.name}:{goal.isChecked}</p>)}
+        </SC.GoalsListContainer>
+        { errors.request && <SC.Error>{ errors.request }</SC.Error>}
+        { isFetching && <SC.SpinnerContainer><Spinner /></SC.SpinnerContainer>}
+      </SC.GoalsContainer>
+    );
+  }
+}
+
+
+Goals.propTypes = {
+  getDailyGoals: PropTypes.func,
+  dailyGoals: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    isChecked: PropTypes.bool,
+  })),
+  isFetching: PropTypes.bool,
+  errors: PropTypes.shape({}),
+};
+
+Goals.defaultProps = {
+  getDailyGoals: () => {},
+  dailyGoals: [],
+  isFetching: false,
+  errors: {},
+};
+
+
+const mapStateToProps = ({ goals }) => ({
+  dailyGoals: goals.dailyGoals.data,
+  isFetching: goals.dailyGoals.isFetching,
+  errors: goals.dailyGoals.errors,
+});
+
+
+const mapDispatchToProps = dispatch => ({
+  getDailyGoals: () => dispatch(actions.dailyGoals.request()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Goals);
