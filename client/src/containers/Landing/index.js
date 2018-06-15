63f3c01df4328a4c9c50371b2d2c565c2d7781bf
Mariano Mariano <marianofheller@gmail.com>
2018-06-15 17:53:48 -0300

    Landing to functional component

--- client/src/containers/Landing/index.js
+++ client/src/containers/Landing/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 import * as SC from './StyledComponents';
 
-export default class Landing extends React.Component {
-  render() {
-    return (
-      <SC.LandingWrapper>
-        <SC.Title>title</SC.Title>
-        <SC.Subtitle>pwa productivity thingy</SC.Subtitle>
 
-        <SC.ButtonsContainer>
-          <SC.ButtonLink to="/login" text="SIGN IN" />
-          <SC.ButtonLink to="/signup" text="SIGN UP" />
-        </SC.ButtonsContainer>
+const Landing = () => (
+  <SC.LandingWrapper>
+    <SC.Title>title</SC.Title>
+    <SC.Subtitle>pwa productivity thingy</SC.Subtitle>
 
-      </SC.LandingWrapper>
-    );
-  }
-}
+    <SC.ButtonsContainer>
+      <SC.ButtonLink to="/login" text="SIGN IN" />
+      <SC.ButtonLink to="/signup" text="SIGN UP" />
+    </SC.ButtonsContainer>
+
+  </SC.LandingWrapper>
+);
+
+
+export default Landing;
