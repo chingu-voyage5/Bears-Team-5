31f32d13eb4ea3788c752745830aa802fd064a4b
ziggysauce <dtn3423@gmail.com>
2018-06-23 16:13:17 -0400

    Implement CRUD routes for short and long term goals

---
+++ server/api/dailyGoals.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const ShortGoal = require('../models/').shortgoal
+
+/**
+ * GET ROUTE 
+ * READ -- Fetch daily goals
+ */
+router.get('/', async (req, res) => {
+  try {
+    const dailyGoals = await ShortGoal.findAll();
+    if (dailyGoals.length > 0) return res.status(200).send(dailyGoals);
+    return res.status(200).send([{ name: 'No goals created yet!' }]);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+/**
+ * POST ROUTE
+ * CREATE -- Make a new daily goal
+ */
+router.post('/', async (req, res) => {
+  try {
+    const dailyGoal = await ShortGoal.create({
+      id: req.body.id,
+      description: req.body.description,
+      longgoal_id: req.body.longgoal_id,
+    });
+    res.status(201).send(dailyGoal);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+/**
+ * PUT ROUTE
+ * UPDATE -- Edit existing daily goal
+ */
+router.put('/', async (req, res) => {
+  try {
+    const dailyGoal = await ShortGoal.findOne({ where: { id: req.body.id }});
+    if (dailyGoal) {
+      const updateDailyGoal = await ShortGoal.update({
+        description: req.body.description,
+        longgoal_id: req.body.longgoal_id,
+      }, { where: { id: req.body.id }});
+      if (updateDailyGoal) {
+        return res.status(200).send(updateDailyGoal);
+      } else {
+        res.status(500).send({ message: 'Parameters missing' });
+      }
+    } else {
+      res.status(404).send({ message: 'Goal not found' });
+    }
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+/**
+ * DELETE ROUTE
+ * DESTROY -- Remove daily goal
+ */
+router.delete('/', async (req, res) => {
+  try {
+    const dailyGoal = await ShortGoal.findOne({ where: { id: req.body.id }});
+    if (dailyGoal) {
+      dailyGoal.destroy({ force: true });
+      return res.status(200).send({ message: 'Successfully deleted goal' });
+    } else {
+      return res.status(404).send({ message: 'Goal not found' });
+    }
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+module.exports = router;
---
+++ server/api/longTermGoals.js
@@ -0,0 +1,77 @@
+const router = require('express').Router();
+const LongGoal = require('../models/').longgoal
+
+/**
+ * GET ROUTE 
+ * READ -- Fetch long-term goals
+ */
+router.get('/', async (req, res) => {
+  try {
+    const longGoals = await LongGoal.findAll();
+    if (longGoals.length > 0) return res.status(200).send(longGoals);
+    return res.status(200).send([{ name: 'No long-term goals created yet!' }]);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+/**
+ * POST ROUTE
+ * CREATE -- Make a new long-term goal
+ */
+router.post('/', async (req, res) => {
+  try {
+    const longGoal = await LongGoal.create({
+      id: req.body.id,
+      description: req.body.description,
+      user_id: req.body.user_id,
+    });
+    res.status(201).send(longGoal);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+/**
+ * PUT ROUTE
+ * UPDATE -- Edit existing long-term goal
+ */
+router.put('/', async (req, res) => {
+  try {
+    const longGoal = await LongGoal.findOne({ where: { id: req.body.id }});
+    if (longGoal) {
+      const updateLongGoal = await LongGoal.update({
+        description: req.body.description,
+      }, { where: { id: req.body.id }});
+      if (updateLongGoal) {
+        res.status(200).send(updateLongGoal);
+      } else {
+        res.status(500).send({ message: 'Parameters missing' });
+      }
+    } else {
+      res.status(404).send({ message: 'Goal not found' });
+    }
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+/**
+ * DELETE ROUTE
+ * DESTROY -- Remove long-term goal
+ */
+router.delete('/', async (req, res) => {
+  try {
+    const longGoal = await LongGoal.findOne({ where: { id: req.body.id }});
+    if (longGoal) {
+      longGoal.destroy({ force: true });
+      return res.status(200).send({ message: 'Successfully deleted goal' });
+    } else {
+      return res.status(404).send({ message: 'Goal not found' });
+    }
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+module.exports = router;
